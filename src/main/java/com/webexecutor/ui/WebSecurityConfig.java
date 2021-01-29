e716c4933f4c8361327122efdfd4df0486e04529
Kristofer Pearson <pearsonk27@gmail.com>
2021-01-28 20:28:53 -0500

    Security Improvement (stored hashed password)

--- src/main/java/com/webexecutor/ui/WebSecurityConfig.java
+++ src/main/java/com/webexecutor/ui/WebSecurityConfig.java
@@ -31,12 +31,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+
+        // PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        // outputs {bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG
+        // remember the password that is printed out and use in the next step
+        // System.out.println(encoder.encode("password"));
+
+        UserDetails user = User.withUsername("user")
+            .password("{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG")
+            .roles("USER")
+            .build();
 
         return new InMemoryUserDetailsManager(user);
     }
