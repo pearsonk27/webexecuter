532c07d57b9fa6107f5f47f259c53a41ff41c9ec
Kristofer Pearson <pearsonk27@gmail.com>
2021-01-30 20:43:17 -0500

    Unit Test method name change

--- src/test/java/com/webexecutor/ui/UiApplicationUnitTest.java
+++ src/test/java/com/webexecutor/ui/UiApplicationUnitTest.java
@@ -21,7 +21,7 @@ public class UiApplicationUnitTest {
     private MockMvc mvc;
 
     @Test
-    public void getHello() throws Exception {
+    public void testGetHome() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("<!DOCTYPE html>\n" + 
