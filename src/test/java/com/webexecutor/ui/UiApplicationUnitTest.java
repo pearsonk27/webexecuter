5bbc5e5a77847d250df562e42edec6863419917a
Kristofer Pearson <pearsonk27@gmail.com>
2021-01-30 20:03:51 -0500

    Unit Test changes

--- src/test/java/com/webexecutor/ui/UiApplicationUnitTest.java
+++ src/test/java/com/webexecutor/ui/UiApplicationUnitTest.java
@@ -15,7 +15,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class HelloControllerUnitTest {
+public class UiApplicationUnitTest {
 
     @Autowired
     private MockMvc mvc;
@@ -24,6 +24,9 @@ public class HelloControllerUnitTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+                .andExpect(content().string(equalTo("<!DOCTYPE html>\n" + 
+                "<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:sec=\"https://www.thymeleaf.org/thymeleaf-extras-springsecurity3\">\n" +
+                "    <head>\n        <title>Webexecuter</title>\n    </head>\n    <body>\n        <h1>Welcome!</h1>\n" +
+                "        <br />\n        <p>Click <a href=\"/dashboard\">here</a> to go to the dashboard</p>\n    </body>\n</html>")));
     }
 }
