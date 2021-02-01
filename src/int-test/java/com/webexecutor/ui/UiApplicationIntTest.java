eec516fa60b285843f5d31334b63c9259ce19f7c
Kristofer Pearson <pearsonk27@gmail.com>
2021-01-31 21:27:46 -0500

    Change name of integration test

--- src/int-test/java/com/webexecutor/ui/UiApplicationIntTest.java
+++ src/int-test/java/com/webexecutor/ui/UiApplicationIntTest.java
@@ -14,7 +14,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HelloControllerIntTest {
+public class UiApplicationIntTest {
 
     @LocalServerPort
     private int port;
@@ -30,9 +30,12 @@ public class HelloControllerIntTest {
     }
 
     @Test
-    public void getHello() throws Exception {
+    public void testGetHome() throws Exception {
         ResponseEntity<String> response = template.getForEntity(base.toString(),
                 String.class);
-        assertThat(response.getBody()).isEqualTo("Greetings from Spring Boot!");
+        assertThat(response.getBody()).isEqualTo("<!DOCTYPE html>\n" + 
+        "<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:sec=\"https://www.thymeleaf.org/thymeleaf-extras-springsecurity3\">\n" +
+        "    <head>\n        <title>Webexecuter</title>\n    </head>\n    <body>\n        <h1>Welcome!</h1>\n" +
+        "        <br />\n        <p>Click <a href=\"/dashboard\">here</a> to go to the dashboard</p>\n    </body>\n</html>");
     }
 }
